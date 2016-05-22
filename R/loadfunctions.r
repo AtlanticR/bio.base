4cf838561801f4334089fad54b7c06ca98967614
Jae S Choi <choijaeseok@gmail.com>
2016-05-22 19:48:25 -0300

    tweaking to reload libraries

--- R/loadfunctions.r
+++ R/loadfunctions.r
@@ -8,50 +8,50 @@
 
   print( "This function is deprecated... please use ecomodLibrary()." )
   print( "Note that return values are of library names rather than file names." )
-
   require(devtools)
 
   gref = ecomodLibraryList()
+  pkgsLoaded = .packages()
+  pkgsInstalled = .packages(all.available = TRUE)
+
   notinEcomod = setdiff( c(...), gref$libname )
   if (length( notinEcomod) > 0 ) {
     print( "The following are not part of ecomod ... " )
     print( notinEcomod )
   }
 
-  pkgs = rownames(installed.packages())
-  found = intersect( pkgs, c(...) )
+  found = intersect( pkgsInstalled, c(...) )
   if (length(found) > 0 ) {
-    for ( l in found ) {
-      if (is_attached(pkg)) {
-        message("Reloading installed ", pkg$package)
-        unload( inst( pkg) )
+    for ( pkg in found ) {
+      if ( pkg %in% pkgsLoaded ) {
+        message("Reloading installed ", pkg )
+        detach( paste("package", pkg, sep=":"))
       }
-      require( l, character.only=T )
+      require( pkg, character.only = TRUE )
     }
   }
 
-  notfound = setdiff( c( ... ), pkgs )
+  notfound = setdiff( c( ... ), pkgsInstalled )
   if (length(notfound) > 0) {
     print( "Missing some ecomod dependencies...")
     n = readline(prompt="Install them? (y/n): ")
     if ( tolower(n) %in% c("y", "yes") ) {
-      if ( ! "devtools" %in% pkgs ) install.packages( "devtools", dependencies=TRUE )
+      if ( ! "devtools" %in% pkgsInstalled ) install.packages( "devtools", dependencies=TRUE )
       for ( nf in notfound ) {
         oo = which( gref$libname == nf )
         if (length(oo)==1) {
           try( install_github( gref$githubLoc[oo] ) )
-          pkg = as.package(gref$libname[oo])
-          if (is_attached(pkg)) {
-            message("Reloading installed ", pkg$package)
-            unload( inst( pkg) )
+          pkg = gref$libname[oo]
+          if ( pkg %in% pkgsLoaded ) {
+            message("Reloading installed ", pkg )
+            detach( paste("package", pkg, sep=":"))
           }
-          require( pkg$package, character.only = TRUE )
+          require( pkg, character.only = TRUE )
         }
       }
     }
   }
   return( c(...) )
 }
-
 
 

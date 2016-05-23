920a5890bb059f83654df6e9c7a614fea0d6d3b1
Jae S Choi <choijaeseok@gmail.com>
2016-05-23 19:26:45 -0300

    add namespace/library clearing mechanism

--- R/ecomodLibraryInstall.r
+++ R/ecomodLibraryInstall.r
@@ -1,11 +1,11 @@
-ecomodLibraryInstall = function() {
+ecomodLibraryInstall = function(...) {
+  #\\ add install_github flags e.g. force=TRUE to call if desired
   pkgsInstalled = .packages(all.available = TRUE)
   if ( ! "devtools" %in% pkgsInstalled ) install.packages( "devtools", dependencies=TRUE )
   require(devtools)
-
   gref = ecomodLibraryList()
   for ( pkg in gref$githubLoc ) {
-    try( install_github( pkg ) )
+    try( install_github( pkg, ... ) )
   }
 }
 

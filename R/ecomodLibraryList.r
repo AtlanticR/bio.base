0de6d6bd7cafc7204d1f18274f224239d001d14f
Jae S Choi <choijaeseok@gmail.com>
2016-05-26 15:44:42 -0300

    update package list

--- R/ecomodLibraryList.r
+++ R/ecomodLibraryList.r
@@ -3,6 +3,7 @@ ecomodLibraryList = function() {
 
   elibs = data.frame( rbind(
     cbind( "ecomodUtils", "jae0/ecomodUtils"),
+    cbind( "utilities", "jae0/utilities"),
     cbind( "groundfish", "jae0/groundfish"),
     cbind( "netmensuration", "jae0/netmensuration"),
     cbind( "snowcrab", "jae0/snowcrab")

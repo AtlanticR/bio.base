e95488442d825c538e1fea6d1251eb4723debf01
Jae S Choi <choijaeseok@gmail.com>
2016-05-31 14:47:28 -0300

    name changes

---
+++ R/bioLibraryList.r
@@ -0,0 +1,20 @@
+
+bioLibraryList = function() {
+
+  elibs = data.frame( rbind(
+    cbind( "bio.base", "Beothuk/bio.base"),
+    cbind( "bio.bathymetry", "Beothuk/bio.bathymetry"),
+    cbind( "bio.coastline", "Beothuk/bio.coastline"),
+    cbind( "bio.polygons", "Beothuk/bio.polygons"),
+    cbind( "bio.substrate", "Beothuk/bio.substrate"),
+    cbind( "bio.utilities", "Beothuk/bio.utilities"),
+    cbind( "groundfish", "jae0/groundfish"),
+    cbind( "netmensuration", "jae0/netmensuration"),
+    cbind( "snowcrab", "jae0/snowcrab")
+  ), stringsAsFactors=FALSE)
+
+  names(elibs) = c( "libname", "githubLoc" )
+  return(elibs)
+}
+
+

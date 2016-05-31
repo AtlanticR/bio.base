cc6f7bafb6c924f301dfaf488edb6434e302aedd
Jae S Choi <choijaeseok@gmail.com>
2016-05-31 10:51:38 -0300

    misc

--- R/ecomodLibraryList.r
+++ R/ecomodLibraryList.r
@@ -3,7 +3,10 @@ ecomodLibraryList = function() {
 
   elibs = data.frame( rbind(
     cbind( "ecomod_base", "jae0/ecomod_base"),
-    cbind( "utilities", "jae0/utilities"),
+    cbind( "ecomod_coastline", "jae0/ecomod_coastline"),
+    cbind( "ecomod_polygons", "jae0/ecomod_polygons"),
+    cbind( "ecomod_substrate", "jae0/ecomod_substrate"),
+    cbind( "ecomod_utilities", "jae0/ecomod_utilities"),
     cbind( "groundfish", "jae0/groundfish"),
     cbind( "netmensuration", "jae0/netmensuration"),
     cbind( "snowcrab", "jae0/snowcrab")
